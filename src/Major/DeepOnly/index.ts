dd198792340a96dbd38cd8e8ee77e586484884b7
charlotte-z <2698028166@qq.com>
2022-09-08 14:56:46 +0800

    add comment

--- src/Major/DeepOnly/index.ts
+++ src/Major/DeepOnly/index.ts
@@ -1,5 +1,21 @@
 /**
  * Read only each object
+ * 
+ * e.g.
+    const test: DeepReadOnly<{
+        a: {
+            b: string;
+            c: any[]
+        }
+    }> = {
+        a: {
+            b: '',
+            c: ['1', 2]
+        }
+    }
+
+    test.a.c[0] = 1 // Index signature in type 'readonly any[]' only permits reading
+    test.a.b = 'new data' // Cannot assign to 'b' because it is a read-only property
  */
 
 export declare type DeepReadOnly<O> = {
